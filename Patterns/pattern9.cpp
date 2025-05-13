b693242b6a5f3c438faaa02aeae36069bbffe6da
Your Name <you@example.com>
2025-05-14 03:33:18 +0530

    pattern 9

---
+++ Patterns/pattern9.cpp
@@ -0,0 +1,29 @@
+/*
+1
+01
+101
+0101
+10101
+*/
+#include<iostream>
+using namespace std;
+int main(){
+int n = 5;
+int start = 0;   // any value we can put initially i will going to change as per the condition
+for(int i = 1; i<=n;i++){
+    if(i%2==0){
+            start =0;
+        }
+        else{
+            start =1;
+        }
+    for(int j = 1; j<=i; j++){
+        
+        cout<<start;
+        start=1 - start;
+        
+    }
+    cout<<endl;
+}
+ 
+}

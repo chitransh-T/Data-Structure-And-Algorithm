5593cf01e1ae95d10e7e271e98257979c74781f7
Your Name <chitranshtiwari78@gmail.com>
2025-10-23 23:57:00 +0530

    reverse function change

--- Arrays/ReverseArray.cpp
+++ Arrays/ReverseArray.cpp
@@ -18,7 +18,7 @@ void reverseArray(int arr[] ,int size){
 int main(){
   int arr[] = {2,6,3,9,5};
   int size = sizeof(arr)/sizeof(arr[0]);
-  reverseArray(arr,size);                      
+  reverseArray(arr,size);                     // calling the reverese array function 
   
   for(int i =0 ; i<size; i++){                // printing the array after reverseing
       cout<<arr[i]<<" ";
